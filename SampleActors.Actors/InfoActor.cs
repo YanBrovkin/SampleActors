78ce100afef669f30b99b0d990d7417c87648627
Yan Brovkin <brovkin.yan@gmail.com>
2016-12-09 19:37:31 +0200

    Initial

---
+++ SampleActors.Actors/InfoActor.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using System;
+
+namespace SampleActors.Actors
+{
+    public class InfoActor: ReceiveActor
+    {
+        public class Info
+        {
+            public Info(string from)
+            {
+                Informer = from;
+            }
+            public string Informer { get; }
+        }
+
+        public InfoActor()
+        {
+            //Receive<Info>(info => Sender.Tell("Hello " + info.Informer, Self));
+            Receive<Info>(info => Console.WriteLine("Hello {0}", info.Informer));
+        }
+    }
+}
---
+++ SampleActors.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using SampleActors.Actors;
+using System;
+
+namespace SampleActors.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("MySystem");
+
+            var informer = system.ActorOf<InfoActor>("informer");
+
+            informer.Tell(new InfoActor.Info("First"));
+            informer.Tell(new InfoActor.Info("Next"));
+
+            Console.ReadLine();
+        }
+    }
+}
